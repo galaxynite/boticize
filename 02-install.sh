f2ecacfa2886fccf3f731b19fc53080190661c6b
galaxynite <37035397+galaxynite@users.noreply.github.com>
2018-04-25 21:16:27 +0800

    4.8 Botic 7

--- 02-install.sh
+++ 02-install.sh
@@ -3,20 +3,7 @@
 ### Install packages
 apt-get update
 apt-get install --no-install-recommends -y \
-	sox libsox-fmt-all \
-	alsa-utils \
-	firmware-realtek firmware-ralink \
-	ntpdate \
-	openssh-server openssh-client \
-	cifs-utils \
-	nfs-common \
-	dbus wireless-tools wpasupplicant \
-	i2c-tools \
-	acpid \
-	cpufrequtils usbutils \
-	less psmisc \
-	python-alsaaudio \
-	lirc \
-	linux-image-4.5.0-botic7-rc1 \
-	linux-libc-dev=4.5-botic7-rc1_armhf \
-	linux-headers-4.5.0-botic7-rc1
+	
+	linux-image-4.8.13-botic7-rc3_1_armhf.deb \
+	linux-libc-dev_4.5-botic7-rc1_armhf.deb \
+	linux-headers-4.8.13-botic7-rc3_1_armhf.deb
