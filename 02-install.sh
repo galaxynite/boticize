11cb260a9d71fa8518e9cc14f2c9d5510872e409
galaxynite <37035397+galaxynite@users.noreply.github.com>
2018-04-26 21:51:58 +0800

    Update 02-install.sh

--- 02-install.sh
+++ 02-install.sh
@@ -3,7 +3,6 @@
 ### Install packages
 apt-get update
 apt-get install --no-install-recommends -y \
-	
 	linux-image-4.8.13-botic7-rc3_1_armhf.deb \
 	linux-libc-dev_4.5-botic7-rc1_armhf.deb \
 	linux-headers-4.8.13-botic7-rc3_1_armhf.deb
